65877a97e57e159f29d45553fae816420859121d
voznov <voznov1peter@gmail.com>
2018-12-17 04:24:39 +0300

    lab3

--- algo/3/A.cpp
+++ algo/3/A.cpp
@@ -2,3 +2,16 @@
 // Created by admin on 12/17/18.
 //
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifndef debug
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    return 0;
+}
